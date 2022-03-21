2e3c325a8b86691d959dda78371c2afe5ab01c67
Shubham <shubhampatel2295@gmail.com>
2022-03-21 17:32:30 -0600

    Implemented delete url functionality

--- express_server.js
+++ express_server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 const PORT = 8080; //default port 8080
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
 const generateRandomString = function() {
-
-  let result = "kas";
-  for (let i = 0; i < 3; i++) {
-    const randomNumber = Math.trunc(Math.random() * 9);
-    result += randomNumber;
-  }
-
-  return result;
-
+  return Math.random().toString(36).substring(2, 8);
 };
 
 const urlDatabase = {
@@ -27,24 +21,37 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-});
+app.route('/urls')
+  .get((req, res) => {
+    const templateVars = {urls: urlDatabase};
+    res.render("urls_index", templateVars);
+  })
+  .post((req, res) => {
+    const randomShortUrl = generateRandomString();
+    urlDatabase[randomShortUrl] = req.body.longURL;
+    res.redirect(`/urls/${randomShortUrl}`);
+  });
 
 app.get("/urls/new", (req, res) => {
-  //const templateVars = 0;
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls.json', (req, res) => {
